43d61b39d9789c62c33dca3193280bc0d2a22b2c
Tyler Davis <tgfdavis98@gmail.com>
2020-06-27 02:15:17 +0100

    add boilerplate meme controller.

---
+++ Wizard/Controllers/MemeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using Wizard.Structures;
+
+namespace Wizard.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class MemeController : ControllerBase
+	{
+		[HttpGet("test")]
+		public IActionResult Get([FromBody] SlapData data)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Wizard/Structures/SlapData.cs
@@ -0,0 +1,14 @@
+﻿namespace Wizard.Structures
+{
+	public readonly ref struct SlapData
+	{
+		public readonly string FirstUser;
+		public readonly string SecondUser;
+
+		public SlapData(string firstUser, string secondUser)
+		{
+			FirstUser = firstUser;
+			SecondUser = secondUser;
+		}
+	}
+}
