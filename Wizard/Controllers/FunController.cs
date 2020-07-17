ddc33b3385ad8dacb76fe227a2d0b7f413955ee1
Tyler Davis <tgfdavis98@gmail.com>
2020-07-17 17:13:04 +0100

    cleanup code

--- Wizard/Controllers/FunController.cs
+++ Wizard/Controllers/FunController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Net.Http.Headers;
+using SixLabors.Fonts;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Drawing;
 using SixLabors.ImageSharp.Drawing.Processing;
@@ -30,35 +31,72 @@ namespace Wizard.Controllers
 			{0.2f, 7}
 		};
 
+		private readonly TextGraphicsOptions _options = new TextGraphicsOptions
+		{
+			TextOptions = new TextOptions
+			{
+				HorizontalAlignment = HorizontalAlignment.Center,
+				VerticalAlignment = VerticalAlignment.Center
+			}
+		};
+
 		private readonly Polygon _trianglePolygon = new Polygon(new LinearLineSegment(
 			new PointF(250, 50),
 			new PointF(240, 35),
 			new PointF(260, 35)));
 
-		[HttpGet("wof")]
-		public async Task<IActionResult> GetWofAsync([FromQuery] float winningAmount)
+		private readonly Color _triangleColour = Color.ParseHex("#FFCC33");
+
+		private readonly FontFamily _textFont =
+			new FontCollection().Install("./Assets/fonts/Roboto-Light.ttf");
+
+		private readonly Random _random = new Random();
+		private readonly Font _topFont;
+		private readonly Font _bottomFont;
+		private readonly PointF _topLocation = new PointF(600, 200);
+		private readonly PointF _bottomLocation = new PointF(600, 300);
+		private readonly Image _background = Image.Load(FileSystem.OpenRead("./Assets/images/fun/wof_background.bmp"));
+		private readonly Image _wheel = Image.Load(FileSystem.OpenRead("./Assets/images/fun/wof.bmp"));
+		private readonly Image _shiny = Image.Load(FileSystem.OpenRead("./Assets/images/social/shiny_small.png"));
+
+		public FunController()
 		{
-			using var image = new Image<Rgb24>(800, 500);
-			using var wheel = await Image.LoadAsync(FileSystem.OpenRead("./Assets/images/fun/wof.bmp"));
+			_topFont = _textFont.CreateFont(46f);
+			_bottomFont = _textFont.CreateFont(32f);
+		}
 
-			var random = new Random();
-			var rnd = random.NextDouble();
+		[HttpGet("wof")]
+		public async Task<IActionResult> GetWofAsync([FromQuery] float winningAmount, [FromQuery] int bet)
+		{
+			var rnd = _random.NextDouble();
 
-			var offset = (45f * _winningRotations[winningAmount]);
+			var offset = 45f * _winningRotations[winningAmount];
 
 
 			var rotationAmount = offset + (43f * (float)rnd) + 1f;
-			wheel.Mutate(context => { context.Rotate(-rotationAmount); });
+			_wheel.Mutate(context => { context.Rotate(-rotationAmount); });
 
-			image.Mutate(context =>
+			var won = winningAmount > 1f;
+			var text = won ? "Won!" : "Lost.";
+
+			var updatedBalance = bet * winningAmount;
+
+			var measure = TextMeasurer.MeasureBounds($"New Balance: {updatedBalance}", new RendererOptions(_textFont.CreateFont(32f)));
+
+			_background.Mutate(context =>
 			{
-				context.DrawImage(wheel, new Point(250 - (wheel.Width / 2), 250 - (wheel.Height / 2)), 1f);
-				context.Fill(Color.Gold, _trianglePolygon);
+				context.DrawImage(_wheel, new Point(250 - (_wheel.Width / 2), 250 - (_wheel.Height / 2)), 1f);
+				context.Fill(_triangleColour, _trianglePolygon);
+				context.DrawText(_options, $"You {text}", _topFont, Color.White, _topLocation);
+				context.DrawText(_options, $"New Balance: {updatedBalance}", _bottomFont, Color.LightGray, _bottomLocation);
+				context.DrawImage(_shiny, new Point(610 + (int)measure.Width / 2, 300 - 14), 1f);
 			});
+
+
 
 			var stream = new MemoryStream();
 
-			await image.SaveAsync(stream, Encoder);
+			await _background.SaveAsync(stream, Encoder);
 
 			stream.Seek(0, SeekOrigin.Begin);
 
